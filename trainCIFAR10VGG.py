fd4977158c31c0154e62d33e50be87047e0cde9c
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2020-05-08 11:31:12 +0800

    modify training scripts

--- trainCIFAR10VGG.py
+++ trainCIFAR10VGG.py
@@ -137,12 +137,10 @@ def main():
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.ToTensor()
     ])
     transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.ToTensor()
     ])
     trainset = datasets.CIFAR10(root=args.data_folder, train=True, download=True, transform=transform_train)
     testset = datasets.CIFAR10(root=args.data_folder, train=False, download=True, transform=transform_test)
