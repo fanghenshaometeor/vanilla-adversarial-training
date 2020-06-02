4a641474112d6608d447996596cd0cc436a93396
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2020-06-02 11:13:20 +0800

    add comments

--- attack.sh
+++ attack.sh
@@ -1,4 +1,4 @@
-# -----------------------------------------------
+# --------- CIFAR10-vgg16/resnet18 --------------
 # model=vgg16
 # model=resnet18
 # model_path='./save/CIFAR10-VGG.pth'
@@ -7,7 +7,7 @@
 # model_path='./save/CIFAR10-Resnet18-adv.pth'
 # dataset=CIFAR10
 # data_dir='/media/Disk1/KunFang/data/CIFAR10/'
-# -----------------------------------------------
+# --------- STL10-modelA ------------------------
 model=aaron
 # model_path='./save/STL10-aaron.pth'
 model_path='./save/STL10-aaron-adv.pth'
@@ -18,7 +18,7 @@ gpu_id=1
 
 python attack.py \
     --model ${model} \
+    --model_path ${model_path} \
     --dataset ${dataset} \
     --data_dir ${data_dir}   \
-    --model_path ${model_path} \
     --gpu_id ${gpu_id}
